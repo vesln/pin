90166d0310cb32cadbfad5d91ab79e587d6ae3aa
Veselin Todorov <veselin@veselin.bg>
2012-04-22 14:24:45 +0300

    Fix status code check

--- test/pin.test.js
+++ test/pin.test.js
@@ -29,39 +29,43 @@ describe('pin', function() {
   describe('when monitoring', function() {
     it('is repeatable', function(done) {
       var driver = fakeDriver(function(url, fn) {
-        fn(null, {statuscode: 200});
+        fn(null, {statusCode: '200'});
       });
 
       var i = 0;
 
       pin('http://google.com/', driver)
-       .interval(5)
-       .up(function(res) {
+        .interval(5)
+        .up(function(res) {
           if (++i == 2) done();
         });
     });
 
-    it('notifies when site is down', function() {
+    it('notifies when site is down', function(done) {
       var driver = fakeDriver(function(url, fn) {
-        fn(null, {statuscode: 404});
+        fn(null, {statusCode: 404});
       });
 
+      var end = false;
+
       pin('http://google.com/', driver)
-       .interval(5)
-       .down(function(err, res) {
-          done();
+        .interval(5)
+        .down(function(err, res) {
+          if (!end) (end = true) && done();
         });
     });
 
     it('notifies when site is up', function(done) {
       var driver = fakeDriver(function(url, fn) {
-        fn(null, {statuscode: 200});
+        fn(null, {statusCode: 200});
       });
 
+      var end = false;
+
       pin('http://google.com/', driver)
-       .interval(10)
-       .up(function(err, res) {
-          done();
+        .interval(10)
+        .up(function(err, res) {
+          if (!end) (end = true) && done();
         });
     });
 
@@ -71,10 +75,10 @@ describe('pin', function() {
       });
 
       pin('http://google.com/', driver)
-       .interval(5)
-       .down(function(err, res) {
-         err.should.be.ok;
-         done();
+        .interval(5)
+        .down(function(err, res) {
+          err.should.be.ok;
+          done();
         });
     });
   });
